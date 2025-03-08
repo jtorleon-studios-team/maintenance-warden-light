c4363e19cb76cda4c81d26c8f18687a14be315a4
tmslpm <thomaslepomme@gmail.com>
2025-03-08 09:28:29 +0100

    save

--- src/main/utils/helpers.ts
+++ src/main/utils/helpers.ts
@@ -1,3 +1,23 @@
+/**
+ * ## title
+ * 
+ * ### Feature :
+ * 
+ * - 
+ * 
+ * ### Intro :
+ *  
+ * ```ts
+ * import * as Task from "..."
+ * ```
+ *  
+ * #### Call to Action :
+ * 
+ *  Have questions? Need help? Join us on Discord or visit our GitHub 
+ *  to get involved with the community and contribute! 
+ * 
+ * @packageDocumentation
+ */
 
 import os from 'node:os';
 import path from 'node:path';
